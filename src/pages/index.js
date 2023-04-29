74b2ff66aefcf4d0b71adcd200cebe04095cfcbd
newturok <newturok@gmail.com>
2023-04-29 06:58:38 +0300

    add activities

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/activities">
             Напрямки діяльності
           </Link>
         </div>
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Сторінка Директорату розвитку системи електронних реєстрів Мінцифри">
       <HomepageHeader />
       <main>
         {/* <HomepageFeatures /> */}
